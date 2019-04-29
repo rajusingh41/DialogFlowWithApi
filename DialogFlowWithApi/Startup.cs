af5b26fadac8f84eedee18ffc1cb4f1576fc94d9
rajusingh41 <raju.singh41@gmail.com>
2019-04-29 15:41:44 +0530

    Modify

--- DialogFlowWithApi/Startup.cs
+++ DialogFlowWithApi/Startup.cs
@@ -19,14 +19,15 @@ namespace DialogFlowWithApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("V1", new  Microsoft.OpenApi.Models.OpenApiInfo
-                {
-                    Title = "Dialogflow Bot",
-                    Version = "V1"
-                });
-            });
+            services.AddSwaggerDocumentation();
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("V1", new  Microsoft.OpenApi.Models.OpenApiInfo
+            //    {
+            //        Title = "Dialogflow Bot",
+            //        Version = "V1"
+            //    });
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,12 +43,13 @@ namespace DialogFlowWithApi
             }
             app.UseStaticFiles();
             app.UseHttpsRedirection();
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/V1/swagger.json", "Dialogflow Bot");
-                c.RoutePrefix = string.Empty;
-            });
+            app.UseSwaggerDocumentation();
+            //app.UseSwagger();
+            //app.UseSwaggerUI(c =>
+            //{
+            //    c.SwaggerEndpoint("/swagger/V1/swagger.json", "Dialogflow Bot");
+            //    c.RoutePrefix = string.Empty;
+            //});
             app.UseMvc();
         }
     }
---
+++ DialogFlowWithApi/SwaggerServiceExtensions.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System.Collections.Generic;
+
+namespace DialogFlowWithApi
+{
+    /// <summary>
+    /// SwaggerServiceExtensions
+    /// </summary>
+    public static class SwaggerServiceExtensions
+    {
+        /// <summary>
+        /// AddSwaggerDocumentation
+        /// https://github.com/domaindrivendev/Swashbuckle.AspNetCore/blob/master/README-v5.md
+        /// https://ppolyzos.com/2017/10/30/add-jwt-bearer-authorization-to-swagger-and-asp-net-core/
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("V1.0", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Dialogflow Bot",
+                    Version = "V1.0"
+                });
+
+                c.AddSecurityDefinition("Bearer",
+                    new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                    {
+                        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.ApiKey
+                    });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                        {
+                            {
+                                new OpenApiSecurityScheme
+                                {
+                                    Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+                                },
+                                new[] { "readAccess", "writeAccess" }
+                            }
+                        });
+            });
+            return services;
+        }
+
+        /// <summary>
+        /// UseSwaggerDocumentation
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/V1.0/swagger.json", "Dialogflow Bot");
+                c.RoutePrefix = string.Empty;
+                c.DocExpansion(DocExpansion.None);
+            });
+            return app;
+        }
+
+    }
+}
