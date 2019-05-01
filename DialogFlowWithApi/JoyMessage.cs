92326b0426db2b7deac7ba5669d15295df13dff0
rajusingh41 <raju.singh41@gmail.com>
2019-05-01 18:29:14 +0530

    add joy message

---
+++ DialogFlowWithApi/JoyMessage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DialogFlowWithApi
+{
+    public class JoyMessage
+    {
+    }
+
+    public class ResponseMessage
+    {
+        public ICollection<ModuleMessage> ModuleMessage { get; set; }
+    }
+
+    public class ModuleMessage
+    {
+        public int ModuleId { get; set; }
+
+        public string Comment { get; set; }
+
+        public ICollection<ProcessTypeMessage> ProcessTypeMessage { get; set; }
+    }
+
+    public class ProcessTypeMessage
+    {
+        public int ProcessId { get; set; }
+        public string Comment { get; set; }
+
+        public ICollection<Message> Messages { get; set; }
+    }
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
